004e26da7744e69f507dd5dd3e078239eee0be09
Josh Tucker <joshuatucker648@gmail.com>
2024-02-04 18:34:08 +0100

    Added functions that swap a - for a + in the first item of a list if there is a list above it that starts with -

--- main.ts
+++ main.ts
@@ -1,15 +1,58 @@
-import { Plugin } from 'obsidian';
+import { Editor, Plugin } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
 
 	async onload() {
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		function isListElement(line: string) {
+			const listPrefixes = ["- ", "+ ", "* "];
+			line = line.trimStart();
+			return listPrefixes.some(prefix => line.startsWith(prefix));
+		}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		function isFirstListElement(editor: Editor, lineNumber: number) {
+			if (isListElement(editor.getLine(lineNumber))) {
+				if (lineNumber == 0) {
+					return true;
+				} else {
+					return !isListElement(editor.getLine(lineNumber-1));
+				}
+			} else {
+				return false;
+			}
+		}
+
+		function getPreviousListCharacter(editor: Editor, lineNumber: number) {
+			lineNumber -= 1;
+			while (lineNumber >= 0) {
+				const line: string = editor.getLine(lineNumber);
+				if (isListElement(line)) {
+					return line.trimStart()[0];
+				} else if (line == "") {
+					lineNumber -= 1;
+					continue;
+				} else {
+					return null;
+				}
+			}
+
+			return null;
+		}
+
+		this.registerDomEvent(document, 'keyup', e => {
+			const editor: Editor | undefined = this.app.workspace.activeEditor?.editor;
+			if (editor !== undefined) {
+				const lineNumber: number = editor.getCursor().line;
+				const line: string = editor.getLine(lineNumber);
+				
+				if (isFirstListElement(editor, lineNumber)) {
+					const prevListPrefix: string | null = getPreviousListCharacter(editor, lineNumber);
+					if (prevListPrefix != null) {
+						if (prevListPrefix == "-") {
+							editor.setLine(lineNumber, line.replace("-", "+"));
+						}
+					}
+				}
+			}
+		});
 	}
 }
