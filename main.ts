985df72ad3b38b6eeb81e3172c866fc1dc3388b6
Josh Tucker <joshuatucker648@gmail.com>
2024-02-16 13:43:02 +0000

    Almost complete rewrite for much more flexibility

--- main.ts
+++ main.ts
@@ -9,33 +9,180 @@ export default class ConsecutiveLists extends Plugin {
 			return listPrefixes.some(prefix => line.startsWith(prefix));
 		}
 
-		function isFirstListElement(editor: Editor, lineNumber: number) {
-			if (isListElement(editor.getLine(lineNumber))) {
-				if (lineNumber == 0) {
-					return true;
-				} else {
-					return !isListElement(editor.getLine(lineNumber-1));
-				}
-			} else {
-				return false;
-			}
-		}
-
 		function getPreviousListCharacter(editor: Editor, lineNumber: number) {
-			lineNumber -= 1;
+			let listEnded: boolean = false;
+
 			while (lineNumber >= 0) {
 				const line: string = editor.getLine(lineNumber);
-				if (isListElement(line)) {
-					return line.trimStart()[0];
+				if (isListElement(line) && listEnded) {
+					return (line[0]);
 				} else if (line == "") {
-					lineNumber -= 1;
+					listEnded = true;
+				} else if (!isListElement(line)) {
+					return null;
+				}
+				lineNumber--;
+			}
+
+			return null;
+		}
+
+		function getNextListCharacter(editor: Editor, lineNumber: number) {
+			const numLines: number = editor.getValue().split("\n").length;
+
+			let listEnded: boolean = false;
+
+			while (lineNumber < numLines) {
+				const line: string = editor.getLine(lineNumber);
+				if (isListElement(line) && listEnded) {
+					return (line[0]);
+				} else if (line == "") {
+					listEnded = true;
+				} else if (!isListElement(line)) {
+					return null;
+				}
+				lineNumber++;
+			}
+
+			return null;
+		}
+
+		function indexOfListCharacter(line: string) {
+			for (let i = 0; i < line.length; i++) {
+				if (line[i] == " " || line[i] == "\t") {
 					continue;
 				} else {
-					return null;
+					return i;
 				}
 			}
 
 			return null;
+		}
+
+		function getListChar(editor: Editor, lineNumber: number) {
+			if (lineNumber < 0) return null;
+
+			const line = editor.getLine(lineNumber);
+			const i: number | null = indexOfListCharacter(line);
+
+			if (i == null) {
+				return null;
+			} else {
+				return line[i];
+			}
+		}
+
+		function inconsistencyWithinList(editor: Editor, lineNumber: number) {
+
+			const listChar: string | null = getListChar(editor, lineNumber);
+			const prevListChar: string | null = getListChar(editor, lineNumber-1);
+			const nextListChar: string | null = getListChar(editor, lineNumber+1);
+
+			if (listChar != null && (prevListChar != null || nextListChar != null)) {
+
+				if (prevListChar != null && nextListChar != null) {
+					if (listChar != prevListChar || listChar != nextListChar) {
+						return true;
+					}
+				}
+
+				else if (prevListChar != null) {
+					if (listChar != prevListChar) {
+						return true;
+					}
+				}
+
+				else if (nextListChar != null) {
+					if (listChar != nextListChar) {
+						return true;
+					}
+				}
+			}
+
+			return false;
+		}
+
+		function inconsistencyBetweenLists(editor: Editor, lineNumber: number) {
+
+			const listChar: string | null = getListChar(editor, lineNumber);
+			const prevListChar: string | null = getPreviousListCharacter(editor, lineNumber);
+			const nextListChar: string | null = getNextListCharacter(editor, lineNumber);
+
+			if (listChar != null && (prevListChar != null || nextListChar != null)) {
+
+				if (prevListChar != null && nextListChar != null) {
+					if (listChar == prevListChar || listChar == nextListChar) {
+						return true;
+					}
+				}
+
+				else if (prevListChar != null) {
+					if (listChar == prevListChar) {
+						return true;
+					}
+				}
+
+				else if (nextListChar != null) {
+					if (listChar == nextListChar) {
+						return true;
+					}
+				}
+			}
+
+			return false;
+		}
+
+		function reformatConsecutiveLists(editor: Editor, lineNumber: number) {
+			// console.log("reformat")
+			
+			while (lineNumber > 0) {
+				const line: string = editor.getLine(lineNumber)
+				if (line == "" || isListElement(line)) {
+					lineNumber -= 1;
+				} else {
+					lineNumber += 1;
+					break;
+				}
+			}
+
+			const numLines: number = editor.getValue().split("\n").length;
+			let dashes: boolean = true;
+			let firstList: boolean = true; // this is just because I want the first list to start with '-'
+
+			var line: string = editor.getLine(lineNumber);
+			while ((line == "" || isListElement(line)) && lineNumber < numLines) {
+
+				if (line == "") {
+					if (!firstList) dashes = !dashes;
+
+					while (line == "" && lineNumber < numLines) {
+						lineNumber ++;
+						line = editor.getLine(lineNumber);
+					}
+					lineNumber--;
+
+				} else if (isListElement(line)) {
+					if (dashes) {
+						const i: number | null = indexOfListCharacter(line);
+						if (i != null) {
+							const newLine: string = line.slice(0, i) + '-' + line.slice(i+1);
+							editor.setLine(lineNumber, newLine);
+						}
+					} else {
+						const i: number | null = indexOfListCharacter(line);
+						if (i != null) {
+							const newLine: string = line.slice(0, i) + '+' + line.slice(i+1);
+							editor.setLine(lineNumber, newLine);
+						}
+					}
+				} else {
+					break;
+				}
+
+				firstList = false;
+				lineNumber++;
+				line = editor.getLine(lineNumber);
+			}
 		}
 
 		this.registerDomEvent(document, 'keyup', () => {
@@ -43,16 +190,44 @@ export default class ConsecutiveLists extends Plugin {
 			if (editor !== undefined) {
 				const lineNumber: number = editor.getCursor().line;
 				const line: string = editor.getLine(lineNumber);
-				
-				if (isFirstListElement(editor, lineNumber)) {
-					const prevListPrefix: string | null = getPreviousListCharacter(editor, lineNumber);
-					if (prevListPrefix != null) {
-						if (prevListPrefix == "-" && line.startsWith("-")) {
-							editor.setLine(lineNumber, line.replace("-", "+"));
+				const ch: number = editor.getCursor().ch
+
+				/**
+				 * if a line gets changed when the cursor is not at the end of
+				 * the line, the cursor will be sent back to the start of the line
+				 */
+				if (ch == line.length) {
+
+					if (isListElement(line)) {
+
+						if (inconsistencyBetweenLists(editor, lineNumber)) {
+							// console.log("inconsitency between lists")
+							reformatConsecutiveLists(editor, lineNumber);
+						}
+
+						if (inconsistencyWithinList(editor, lineNumber)) {
+							// console.log("inconsistency within list")
+							reformatConsecutiveLists(editor, lineNumber);
+						}
+
+					}
+
+					if (line.length == 0) {
+
+						const prevListPrefix: string | null = getPreviousListCharacter(editor, lineNumber);
+						const nextListPrefix: string | null = getNextListCharacter(editor, lineNumber);
+
+						if (prevListPrefix != null || nextListPrefix != null) {
+							if (prevListPrefix == nextListPrefix) {
+								// console.log("space reformat")
+								reformatConsecutiveLists(editor, lineNumber);
+							}
 						}
 					}
+
 				}
 			}
 		});
+
 	}
 }
